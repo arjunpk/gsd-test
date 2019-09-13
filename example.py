a4f0789506b9266e3a801dfa168ceb68f3493c20
Arjun Kishore <arjunpk@gmail.com>
2019-09-13 15:03:09 -0400

    Added examples

--- example.py
+++ example.py
@@ -1,13 +1,21 @@
+###############################################################################
+# Date: 13 September 2019                                                     #
+# Author: Arjun Kishore                                                       #
+# This file contains examples and helper methods for classes defined in       #
+# solutions.py                                                                #
+###############################################################################
 from solution import *
 
 import random
 import string
 
+#helper method to generate random string
 def randomString(n=10):
     """Generate a random string of fixed length """
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(n))
 
+# helper method to generate a list of cars
 def generate_cars(n=15):
     car_list = []
     make_model = [['Ford','F150',65000],['Tesla','Model X', 70000],
@@ -20,6 +28,7 @@ def generate_cars(n=15):
         'VA', 'WA', 'WV', 'WI', 'WY', 'PR']
 
     for i in range(n):
+        # Creating a Car object
         car = Car(randomString(9))
         car.year = random.randint(1970, 2020)
         car.make, car.model, car.purchase_value = \
@@ -29,9 +38,21 @@ def generate_cars(n=15):
         car_list.append(car)
     return car_list
 
+# Aggregate a list of cars
+def create_fleet(n=100):
+    return Fleet.agg(generate_cars(n))
+
+#Create an index of cars
+def create_index(n=100):
+    idx = VINIndex()
+    idx.add_car(generate_cars(n))
+    return idx
+
+# Sort list by year
 def sorter_example_by_year(n=50):
     return Car.sort_car_list(generate_cars(n),'year')
 
+# Sort list by state inplace
 def sorter_example_by_state(n=50):
     cars = generate_cars(n)
     Car.sort_car_list(cars,'license_plate_state', True)
